8ea8a107d106d6d94ecb8fe7440bb35729d9e0dd
Psyn3m <150781195+Psyn3m@users.noreply.github.com>
2024-11-09 18:49:47 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Némo Kardassevitch on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
